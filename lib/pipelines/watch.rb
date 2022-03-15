264f5c01ac3d64020c95bb575d7c2ed6652e5830
Spencer Markowski <spencer@theablefew.com>
2022-03-15 01:45:54 -0400

    Add callbacks to watches

--- lib/pipelines/watch.rb
+++ lib/pipelines/watch.rb
@@ -3,7 +3,7 @@ module Pipelines
 
         include Pipelines::Registry
 
-        attr_accessor :name, :at, :input, :every, :destination, :output, :payload, :hook, :parent
+        attr_accessor :name, :at, :input, :every, :destination, :output, :payload, :hook, :parent, :callbacks
 
         def initialize(name, every: nil, at: nil, input: nil, destination: nil , output: nil, parent: nil)
             @name = name
@@ -11,12 +11,23 @@ module Pipelines
             @every = every
             @at = at
             @input = input
+            @callbacks = {}
             @destination = destination || Pipelines::END_OF_QUEUE
             @output = output || :sqs_event_payload
         end
 
         def next_job
             Hash[@destination, Pipelines.build.dig(*@parent)[@destination]]
+        end
+
+        def before(callback, &block)
+            @callbacks[callback] ||= {}
+            @callbacks[callback][:before] = block 
+        end
+
+        def after(callback, &block)
+            @callbacks[callback] ||= {}
+            @callbacks[callback][:after] = block
         end
 
         def build
